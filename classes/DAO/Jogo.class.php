102a399879f76dfa08c20821a05bdaebce1d44f8
Gabriel ribeiro <mazurco066@gmail.com>
2017-09-09 18:07:58 -0300

    Mensagem de Game Over Atualizada

--- classes/DAO/Jogo.class.php
+++ classes/DAO/Jogo.class.php
@@ -1,7 +1,5 @@
 <?php
-
 	class JogoDAO{
-
 		//Atributos
 		private $idUsuario = NULL;
 		private $Atacante = NULL;
@@ -10,19 +8,15 @@
 		private $paisesCp = NULL;
 		private $OrigemCp = NULL;
 		private $AlvoCp = NULL;
-
 		//Método Construtor Padrão da Classe
 		function __construct($id, $PU, $PC, $At, $Df){
-
 			$this->idUsuario = $id;
 			$this->paisesUs = $PU;
 			$this->paisesCp = $PC;
 			$this->Atacante = $At;
 			$this->Defensor = $Df;
 		}
-
 		//Métodos de Classe
-
 		/*
 			Aqui serão definidos todos os métodos
 			que se referem a atacar países e defender.
@@ -31,36 +25,27 @@
 			os 2 vetores ($paisesUs e $paisesCp) para que se
 			manipule os países corretamente para os 2 jogadores.
 		*/
-
 		/**
 		  * @param $paisOrigem indice referente a quem ataca
 		  * @param $paisDefensor indice referente a quem defende
 		  * @return retorna true se conquistou o pais ou false se falhou
 		  */
 		function ocuparPais($paisOrigem, $paisDefensor){
-
 			$tropasAliadas = $this->paisesUs[$paisOrigem]->getTropas();	//Recebe número de tropas das forças aliadas
 			$tropasInimigas = $this->paisesCp[$paisDefensor]->getTropas();	//Recebe número de tropas das forças inimigas
-
 			for ($i = 1; $i <= $tropasAliadas; $i++){
 				//Realizando o ataque ao país inimigo
 				$dado = rand(1, 10);
-
 				if ($dado > 5){
-
 					$tropasInimigas--;
 				}
 				else{
-
 					//Perdeu a tropa vacilão kkkk
 					$tropasAliadas--;
 				}
 			}
-
 			if ($tropasInimigas <= 0){
-
 				$tropasInimigas = 1;	//Ocupa o país deixando um soldado no país ocupado
-
 				//Transferindo o país para o novo dono
 				$tropasAliadas--;
 				if($tropasAliadas <= 0){
@@ -71,178 +56,128 @@
 				$OBJ = $this->paisesCp[$paisDefensor];	//Recuperando o país
 				unset($this->paisesCp[$paisDefensor]);	//Removendo-o do computador
 				$this->paisesUs[] = $OBJ;
-
 				//Retornando Sucesso para o usuário
 				return true;
 			}
 			else{
-
 				//Atualizando Tropas do Inimigo e Usuário
 				$this->paisesUs[$paisOrigem]->setTropas($tropasAliadas);
 				$this->paisesCp[$paisDefensor]->setTropas($tropasInimigas);
 				//Retornando fracasso para o usuário
 				return false;
 			}
-
 		}
-
 		function atacarInimigo(){
-
 			foreach ($this->paisesUs as $key => $value) {
 				
 				if($value->getID() == $this->Atacante){
-
 					//Recuperando informações necessárias
 					$OrigemKey = $key;	//Recupera posição no vetor
 				}
 			}
-
 			foreach ($this->paisesCp as $key => $value) {
 				
 				if($value->getID() == $this->Defensor){
-
 					//Recuperando informações necessárias
 					$DestinoKey = $key;	//Recupera posição no vetor
 					$nomePais = $this->paisesCp[$DestinoKey]->getNome();
 				}
 			}
-
 			if($this->ocuparPais($OrigemKey, $DestinoKey)){	//Caso o ataque ocorrer mas você conquistar o país
-
 				return "Você Conquistou o País Inimigo (".$nomePais.")! <br>";
 			}
 			else{	//Caso o ataque ocorreu mas você perdeu a batalha
-
 				return "Você Falhou em capturar o País (".$nomePais.")! <br>";
 			}
-
 		}
-
 		function tomarPais($paisOrigem, $paisDefensor){
-
 			$tropasAliadas = $this->paisesCp[$paisOrigem]->getTropas();	//Recebe número de tropas das forças aliadas (Computador)
 			$tropasInimigas = $this->paisesUs[$paisDefensor]->getTropas();	//Recebe número de tropas das forças inimigas (usuário)
-
 			for ($i = 1; $i <= $tropasAliadas; $i++){
 				//Realizando o ataque ao país inimigo
 				$dado = rand(1, 10);
-
 				if ($dado > 5){
-
 					$tropasInimigas--;
 				}
 			}
-
 			if ($tropasInimigas <= 0){
-
 				$tropasInimigas = 1;	//Ocupa o país deixando um soldado no país ocupado
-
 				//Transferindo o país para o novo dono
 				$this->paisesUs[$paisDefensor]->setTropas($tropasInimigas);
 				$OBJ = $this->paisesUs[$paisDefensor];	//Recuperando o país
 				unset($this->paisesUs[$paisDefensor]);	//Removendo-o do computador
 				$this->paisesCp[] = $OBJ;
-
 				//Retornando Sucesso para o usuário
 				return true;
 			}
 			else{
-
 				//Atualizando Tropas do Inimigo
 				$this->paisesUs[$paisDefensor]->setTropas($tropasInimigas);
 				//Retornando fracasso para o usuário
 				return false;
 			}
-
-
 		}
-
 		/**
 		  * @param $paisOrigem indice referente a quem ataca
 		  * @param $paisDefensor indice referente a quem defende
 		  * @return retorna true se conquistou o pais ou false se falhou
 		  */
 		function contraAtacar(){
-
 			foreach ($this->paisesCp as $key => $value) {
-
 				if (isset($this->paisesCp[$key])){
 				
 					if($value->getID() == $this->OrigemCp){
-
 						//Recuperando informações necessárias
 						$OrigemKey = $key;	//Recupera posição no vetor
 					}
 				}
 			}
-
 			foreach ($this->paisesUs as $key => $value) {
-
 					if($value->getID() == $this->AlvoCp){
-
 						//Recuperando informações necessárias
 						$DestinoKey = $key;	//Recupera posição no vetor
 						$nomePais = $this->paisesUs[$DestinoKey]->getNome();
 					}
 		
 			}
-
 			if($this->tomarPais($OrigemKey, $DestinoKey)){	//Caso o ataque ocorrer mas você conquistar o país
-
 				return "Computador conquistou seu pais (".$nomePais.")! <br>";
 			}
 			else{	//Caso o ataque ocorreu mas você perdeu a batalha
-
 				return "Computador Falhou em capturar seu País (".$nomePais.")! <br>";
 			}
-
 		}
-
 		function calcularEstrategia($possibilidades){
 			//Verifica qual é a melhor jogada a ser feita
 			$melhor = $possibilidades[0]; //Por padrão a melhor sempre começará sendo a primeira
 			$melhorVantagem = 0;
-
 			foreach ($possibilidades as $key => $value) {
 				
 				$tropasAliadas = $value["tropasAtacante"];
 				$tropasInimigas	= $value["tropasDefensor"];
-
 				if ($tropasAliadas - $tropasInimigas > $melhorVantagem){
-
 					$melhorVantagem = $tropasAliadas - $tropasInimigas;
 					$melhor = $value;
 				}
-
 			}
-
 			//Setando o ataque com melhor vantagem
 			$this->OrigemCp = $melhor["atacante"];
 			$this->AlvoCp = $melhor["defensor"];
-
 		}
-
 		function carregarPossibilidades(){
-
 			$possibilidades = [];
-
 			//Função que analisa qual é a melhor tatica
           	foreach ($this->paisesCp as $key => $value) {
-
 		            $front = $value->getFronteiras();
 		            $listaFronteiras = explode(",", $front);
 		            
 		            foreach ($listaFronteiras as $item => $valor){
-
 		                foreach ($this->paisesUs as $pais => $p) {
-
 		                  $nomePais = $p->getNome();
 		                  $idAtacante = $value->getID();
 		                  $idDefensor = $p->getID();
 		                  $tropasAtacante = $value->getTropas();
 		                  $tropasDefensor = $p->getTropas();
-
-
 		                  if(strcmp($valor, $nomePais) == 0){
 		                    //Possível Ataque
 		                  	$possibilidades[] = [
@@ -256,46 +191,31 @@
 		            }
 	           
           }
-
           return $possibilidades;
          
 		}
-
 		function sortearExercitos(){
 			//Distribui 6 tropas aleatóriamente para os 2 jogadores
-
 			//Definindo quantos países cad ausuário tem
 			$CountUs = count($this->paisesUs) - 1;
 			$CountCp = count($this->paisesCp) - 1;
-
 			for ($i = 0; $i < 6; $i++){
-
 				do{
 					$pos = rand(0, $CountUs);
 					
 				}while(!isset($this->paisesUs[$pos]));
-
 				$this->paisesUs[$pos]->incrementar();
-
 			}
-
 			for ($i = 0; $i < 6; $i++){
-
 				do{
 					$pos = rand(0, $CountCp);
 					
 				}while(!isset($this->paisesCp[$pos]));
-
 				$this->paisesCp[$pos]->incrementar();
-
 			}
-
 			return "---Você Recebeu 6 tropas---<br>---Computador Recebeu 6 tropas---<br>";
-
 		}
-
 		function atualizarDB(){
-
     		
 			//Recuperando Save do jogo no banco de dados
 			$id = $this->idUsuario;
@@ -307,16 +227,12 @@
 		    	
 		    	$row = $rs->fetch(PDO::FETCH_OBJ);
 		    	$idJogo = $row->ID;
-
 		    	if (count($this->paisesUs) > 0){
-
 			      	foreach ($this->paisesUs as $key => $value) {
-
 				        //Definindo Variáveis para o php parar de encher o saco
 				        $tropas = $value->getTropas();
 				        $pertence = "Jogador";
 				        $paisId = $value->getID();
-
 				        $rs = $conexao->prepare("UPDATE status_paises SET tropas = ?, Pertence = ? WHERE Jogo_ID = ? and Pais_ID = ?");
 				        $rs->bindParam(1,$tropas);
 				        $rs->bindParam(2,$pertence);
@@ -325,16 +241,12 @@
 				        $rs->execute();
 			      	}
 			    }
-
 			    if (count($this->paisesCp) > 0){
-
 			      	foreach ($this->paisesCp as $key => $value) {
-
 				        //Definindo Variáveis para o php parar de encher o saco
 				        $tropas = $value->getTropas();
 				        $pertence = "Computador";
 				        $paisId = $value->getID();
-
 				        $rs = $conexao->prepare("UPDATE status_paises SET tropas = ?, Pertence = ? WHERE Jogo_ID = ? and Pais_ID = ?");
 				        $rs->bindParam(1,$tropas);
 				        $rs->bindParam(2,$pertence);
@@ -343,17 +255,12 @@
 				        $rs->execute();
 			      	}
 			    }
-
     		}
     		else{
-
       			//erro ao salvar dados, defeito está na conexão com o banco
       			$flash="OPS!... ouve algum erro em nosso Sistema. Por Favor contate administrador!";
       			echo $flash;
     		}
-
 	}
-
 	}
-
 ?>
--- controllers/ataque.php
+++ controllers/ataque.php
@@ -1,7 +1,6 @@
 <?php
 
 	if($acao=="atacar"){   //Método de aprovar usuário
-
 		//Definindo o relatório de retorno
 		$msg = "RELATÓRIO DE BATALHA <br>";
 		$fim = false;
@@ -9,36 +8,29 @@
         //Recuperando ID's dos paises atacante e defensor
         $Atacante=$_GET["idAtacante"];
         $Defensor=$_GET["idDefensor"];
-
         //Recuperando dados do usuário
         $U = new Usuario();
 		$U->setId($_SESSION["id"]);
-
 		//Instanciando um jogo 
 		$Batalha = new Jogo($U);
-
 		//Verifica se o jogo realmente existe
 		if ($Batalha->checaExistencia() == 1){
-
 			//Carregando Jogo para ver quais países necessitam de alteração
 			$Batalha->carregaJogo();
-
 			//Carrega os países a serem alterados
 			$paisesUs = $Batalha->getPaisesUsuario();
 			$paisesCp = $Batalha->getPaisesComputador();
-
 			//Instanciando Data Acess Object
 			$dao = new JogoDAO($U->getId(),$paisesUs, $paisesCp, $Atacante, $Defensor);
-
 			//Realizando ações ofensivas (OBS - #$!%$#@$%)
 			$msg .= $dao->atacarInimigo();
 				
-			if (count($paisesCp) > 1){	//Se o computador possuir somente um país e ele foi atacado e perdeu end game para computador
+			if (count($paisesCp) > 1){	//Se o computador possuir mais de 1 pais ele vai calcular ataque
 					
 					$ataquesPossiveis = $dao->carregarPossibilidades();
 					$dao->calcularEstrategia($ataquesPossiveis);
 					
-					if (count($paisesUs) > 1){	//Se o computador possuir somente um país e ele foi atacado (com certeza foi) e perdeu end game para usuário
+					if (count($paisesUs) > 1){	//Se o usuário tiver mais de 1 pais ele ira contra atacar
 
 						$msg .= $dao->contraAtacar();
 						$msg .= $dao->sortearExercitos();
@@ -47,26 +39,32 @@
 			}
 			
 			$dao->atualizarDB();
+			if (count($paisesCp) == 1){
 
-			if (count($paisesCp) == 1 || count($paisesUs) == 1){
-
-					$msg .= "<br>---FIM DE JOGO---<br>Uma Nova Partida foi Iniciada para você";
-
+					$msg = "<br>---FIM DE JOGO---<br>---VOCÊ ENCURRALOU OS EXERCÍTOS INIMIGOS---<br>Uma Nova Partida foi Iniciada para você";
 					//Reiniciando o Jogo
 					$id = $U->getId();
 					$conexao = new DB;
 			    	$conexao=$conexao->getConnection();
-
 			    	//Inativando o jogo
 			    	$rs = $conexao->prepare("UPDATE jogos SET emJogo = 0 where id_usuario = ? AND emJogo = 1");
 			    	$rs->bindParam(1,$id);
 			    	$rs->execute();
-
 			}
+			if (count($paisesUs) == 1){
 
+					$msg = "<br>---FIM DE JOGO---<br>---SEU UNICO PAÍS FICOU TOTALMENTE VUNERÁVEL!---<br>Uma Nova Partida foi Iniciada para você";
+					//Reiniciando o Jogo
+					$id = $U->getId();
+					$conexao = new DB;
+			    	$conexao=$conexao->getConnection();
+			    	//Inativando o jogo
+			    	$rs = $conexao->prepare("UPDATE jogos SET emJogo = 0 where id_usuario = ? AND emJogo = 1");
+			    	$rs->bindParam(1,$id);
+			    	$rs->execute();
+			}
 			
 		}        
             
     }
-
 ?>
